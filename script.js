6a5210df2ac2ba19935c2987d0ea7bfa9b0a4d71
Binamin Hassan <136009258+Binkabreezy@users.noreply.github.com>
2023-10-24 06:28:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let playerText = document.getElementById('playerText')
 let restartBtn = document.getElementById('restartBtn')
-let boxes = array.from(document.getElementsByClassName('box'))
+let boxes = Array.from(document.getElementsByClassName('box'))
 
 let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
-
 const O_TEXT = "O"
 const X_TEXT = "X"
 let currentPlayer = X_TEXT
-let spaces = array(9).fill(null)
-
+let spaces = Array(9).fill(null)
+             
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click',boxClicked))
 
@@ -19,8 +18,8 @@ function boxClicked(e) {
     if(!spaces[id]){
         spaces[id] = currentPlayer 
         e.target.innerText = currentPlayer
-        if (playersHasWon() !==false){
-            playerText.innerHTML = `&{currentPlayer} has won!`
+        if (playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} has won!`
             let winning_blocks = playerHasWon()
             winning_blocks.map(box => boxes[box].style.backgroundColor=winnerIndicator)
             return
@@ -67,9 +66,3 @@ function restart() {
 }
 
 startGame()
-
-
-
-
-
-
